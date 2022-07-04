922f33a3c6967138fddcc043692e44cd825e059d
Aishwarya Khatri <75491104+AISHWARYAKHATRI@users.noreply.github.com>
2022-07-05 00:25:00 +0530

    string

---
+++ java20_removeChars.java
@@ -0,0 +1,18 @@
+
+public class java20_removeChars {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		String str = "take12% *&u ^$#forward";
+		for(int i=0; i<str.length(); i++) {
+			int ascii = (int) str.charAt(i);
+			if((ascii >= 60 && ascii <= 90) || (ascii >= 97 && ascii <= 122)) {
+				sb.append(str.charAt(i));
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
+
+// TC: O(N)
+// SC: O(N)
