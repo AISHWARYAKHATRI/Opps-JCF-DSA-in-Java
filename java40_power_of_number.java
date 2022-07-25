39f79a2cee0a026e0f999be49a40e7b607e1e62e
Aishwarya Khatri <75491104+AISHWARYAKHATRI@users.noreply.github.com>
2022-07-25 23:59:42 +0530

    number problem

---
+++ java40_power_of_number.java
@@ -0,0 +1,23 @@
+
+public class java40_power_of_number {
+	public static void main(String args[]) {
+	 int n = 2, k = 3;
+	    int num=n,kk=k;
+	    int ans = 1;
+	    while (k != 0) {
+	      if (k % 2 == 0) {
+	        n = n * n;
+	        k /= 2;
+	      } else {
+	        ans = ans * n;
+	        k--;
+	      }
+	    }
+
+	    System.out.print(num+" raised to the power "+kk+" is "+ans);
+	    }
+}
+
+//Time Complexity: O(logn).
+
+//Space Complexity: O(1)
